907302d7841d5f17d5b878aff50359978d942fe4
norsultani <norsultani@gmail.com>
2025-11-27 11:10:48 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,9 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  codons_and_aminoacids = {}
 
+  for word in rows: 
+   x = word.strip().split("\t")
+   codons_and_aminoacids[x[0]] = x[2]
+
+  return codons_and_aminoacids
 
